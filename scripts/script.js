abea7c4e87a5aac360ce062fac1bb7a54d7c0403
TheMadDrunk <iophamza2@gmail.com>
2022-12-09 19:09:50 +0100

    final with contact

--- scripts/script.js
+++ scripts/script.js
@@ -20,8 +20,8 @@ const options = {
     indicators: {
         activeClasses: 'bg-white dark:bg-gray-800',
         inactiveClasses: 'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
-
     }
+    
 };
 
 const carousel = new Carousel(items, options);
